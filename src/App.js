189dd3d4a2445baeca443cd2508036df97fdd29f
Avaer Kazmer <a@modules.io>
2020-02-11 11:48:30 -0500

    Major refactoring

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Secret from './Secret.js';
-import Token from './Token.js';
-import Mint from './Mint.js';
-import Redeem from './Redeem.js';
-import contract from './contract.js';
+import {xrEnter} from './xr.js';
 
 function App() {
-  contract.init();
-
-  let match, n;
-  if (/^\/secret$/.test(window.location.pathname)) {
-    return (
-      <Secret />
-    );
-  } else if ((match = window.location.pathname.match(/^\/tokens\/([0-9]+)$/)) && (n = parseInt(match[1], 10))) {
-    return (
-      <Token token={n} />
-    );
-  } else if (/^\/mint$/.test(window.location.pathname)) {
-    return (
-      <Mint />
-    );
-  } else if (/^\/redeem$/.test(window.location.pathname)) {
-    return (
-      <Redeem />
-    );
-  } else {
-    return (
-      <pre>has been deployed: an easter egg<br/>
-it comes to those who do not beg<br/>
-its in a place that could be worse<br/>
-so start the hunt inside the verse<br/></pre>
-    );
-  }
+  return <input type="button" value="Enter XR" onClick={() => xrEnter()} />
 }
 
 export default App;
