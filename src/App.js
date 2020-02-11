b826b5a9d136bfa0e705bbe7f8598e19c5597c4e
Avaer Kazmer <a@modules.io>
2020-02-11 12:06:05 -0500

    Add initial branding

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 import {xrEnter} from './xr.js';
 
 function App() {
-  return <input type="button" value="Enter XR" onClick={() => xrEnter()} />
+  return (
+    <div>
+      <header>
+        <img src="logo.svg" alt="logo" height={70} width={70} />
+      </header>
+      <input type="button" value="Enter XR" onClick={() => xrEnter()} />
+    </div>
+  );
 }
 
 export default App;
